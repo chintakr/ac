f903a2a47599f90e5ccc885488af985818c7b83f
chintakr <41940030+chintakr@users.noreply.github.com>
2018-08-18 12:07:27 +0530

    Rename natural.java to natu.java

---
+++ natu.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int k,i,sum=0;
+		 Scanner sc=new Scanner(System.in);
+		 k=sc.nextInt();
+		 for(i=1;i<=k;i++)
+		 {
+		 	sum=sum+i;
+		 }
+		 System.out.println(sum);
+	}
+}
